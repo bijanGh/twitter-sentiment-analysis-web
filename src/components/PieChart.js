c869437599c607deb4d26937e1ba56c5eaf0a75a
bijangh <b.ghanbari.it@gmail.com>
2021-07-23 18:10:53 +0430

    add chart

---
+++ src/components/PieChart.js
@@ -0,0 +1,20 @@
+import { Pie } from "react-chartjs-2";
+import propTypes from "prop-types";
+
+const Chart = ({ data }) => {
+  return data ? <Pie data={data}></Pie> : <></>;
+};
+
+Chart.PropTypes = {
+  data: {
+    lables: propTypes.arrayOf(propTypes.string),
+    datasets: propTypes.arrayOf({
+      label: propTypes.string,
+      data: propTypes.arrayOf(propTypes.number),
+      backgroundColor: propTypes.arrayOf(propTypes.string),
+      hoverOffset: propTypes.number,
+    }),
+  },
+};
+
+export default Chart;
