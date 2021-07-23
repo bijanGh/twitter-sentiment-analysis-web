50c00d8033a69d6c48b9079bc642278a37bd5f9c
bijangh <b.ghanbari.it@gmail.com>
2021-07-23 19:24:27 +0430

    main page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,25 @@
 import Head from "next/head";
 import Image from "next/image";
-import Avatar from "../src/components/Button";
+import TwitterPic from "../public/twitter-240.png";
+
+const data = {
+  labels: ["Red", "Blue", "Yellow"],
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [300, 50, 100],
+      backgroundColor: [
+        "rgb(255, 99, 132)",
+        "rgb(54, 162, 235)",
+        "rgb(255, 205, 86)",
+      ],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+const btnStyles =
+  "px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-600 w-28";
 export default function Home() {
   return (
     <div>
@@ -10,8 +29,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="container flex items-center justify-center h-screen mx-auto align-middle ">
-        <Avatar text="Asghar" />
+      <div className="container flex flex-col items-center justify-center w-full h-screen mx-auto align-middle ">
+        <Image
+          width="100rem"
+          height="100rem"
+          alt="tweeter logo image"
+          src={TwitterPic}
+        />
+        <input
+          className="px-4 py-1 mt-16 text-center border-2 border-blue-500 border-solid rounded w-72 "
+          placeholder="Enter your Twitter username"
+        />
+        <div className="flex flex-row justify-around mt-24 w-72">
+          <button className={`${btnStyles} `}>Mentions</button>
+          <button className={`${btnStyles} `}>Tweets</button>
+        </div>
       </div>
     </div>
   );
