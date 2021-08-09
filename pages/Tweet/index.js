9ca3e32a0a96c71ed5b2097d4973734083881a70
bijangh <b.ghanbari.it@gmail.com>
2021-08-09 21:52:10 +0430

    move out processes

--- pages/Tweet/index.js
+++ pages/Tweet/index.js
@@ -35,9 +35,68 @@ const DefaultElement = ({ children }) => (
   </div>
 );
 
+const Tweets = ({
+  username,
+  userImage,
+  tweetsType,
+  tweetCount,
+  positiveCount,
+  negativeCount,
+  neutralCount,
+  chartData,
+  tweets,
+}) => {
+  return (
+    <div className="min-h-screen bg-indigo-100">
+      <div className="flex flex-wrap items-center justify-center w-full align-middle flex-column">
+        <div className="flex flex-row justify-between w-11/12 pt-3 ">
+          <span className="p-2">
+            <Avatar imageSrc={userImage} userName={username} />
+          </span>
+          <div>
+            <p className="px-1 mt-1 text-lg text-center text-white capitalize bg-blue-600 rounded-lg text-">
+              {tweetsType} : {tweetCount}
+            </p>
+            <p className="px-1 mt-1 text-lg text-green-400 capitalize rounded-lg ">
+              Positive count : {positiveCount}
+            </p>
+            <p className="px-1 mt-1 text-lg text-red-400 capitalize rounded-lg ">
+              Negative count : {negativeCount}
+            </p>
+            <p className="px-1 mt-1 text-lg text-gray-600 capitalize rounded-lg ">
+              Neutral count : {neutralCount}
+            </p>
+          </div>
+
+          <span className="w-32 h-32">
+            <Pie data={chartData} />
+          </span>
+        </div>
+        <div className="flex flex-wrap w-11/12 flex-column ">
+          {tweets.map(({ text, id, score }) => {
+            const bgColor = {
+              NEGATIVE: "bg-red-200",
+              POSITIVE: "bg-green-200",
+              NEUTRAL: "bg-white",
+            };
+
+            return (
+              <span className="w-full py-1 " key={id}>
+                <Tweet text={text} className={bgColor[score]} />
+              </span>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 function Page({}) {
   const router = useRouter();
   const { target, userName } = router.query;
+  const model = useRef(null);
+  const metaData = useRef(null);
   const [TweeterData, setTweeterData] = useState(null);
   const [tweetCount, setTweetCount] = useState(0);
   const [userData, setUserData] = useState(null);
@@ -46,93 +105,81 @@ function Page({}) {
   const [positiveCount, setPositiveCount] = useState(0);
   const [negativeCount, setNegativeCount] = useState(0);
   const [neutralCount, setNeutralCount] = useState(0);
-  const model = useRef(null);
-  const metaData = useRef(null);
-
-  const pageSetup = async (target, userName) => {
-    setStatus("FETCHING_AI");
-
-    if (!model.current) model.current = await loadModel();
-    if (!metaData.current) metaData.current = await getMetaData();
-
-    setStatus("FETCHING_TWEETS");
-
-    const response = await fetch(`${API_URL}/${target}`, {
-      method: "post",
-      body: JSON.stringify({ user: userName }),
-    });
-
-    const tweetsParsed = await response.json();
-    if (!tweetsParsed.tweets || tweetsParsed.tweets.length === 0) {
-      return null;
-    }
-    setTweetCount(tweetsParsed.tweets.length);
-    setStatus("FETCHED_TWEETS");
-
-    let [positiveCount, negativeCount, neutralCount] = [0, 0, 0];
-    const tweetsAnalysed = tweetsParsed.tweets.map((item) => {
-      const score = sentimentAnalysis(
-        item.text,
-        model.current,
-        metaData.current
-      );
-      switch (score) {
-        case "POSITIVE":
-          positiveCount++;
-          break;
-        case "NEGATIVE":
-          negativeCount++;
-          break;
-        default:
-          neutralCount++;
-          break;
-      }
-      return {
-        ...item,
-        score,
-      };
-    });
-    return {
-      user: tweetsParsed.user,
-      tweets: tweetsAnalysed,
-      chart: {
-        datasets: [
-          {
-            label: "sentiment analysis result",
-            data: [positiveCount, negativeCount, neutralCount],
-            backgroundColor: [
-              "rgb(68, 242, 103)",
-              "rgb(240, 104, 77)",
-              "rgb(222, 222, 222)",
-            ],
-            hoverOffset: 4,
-          },
-        ],
-      },
-      positiveCount,
-      negativeCount,
-      neutralCount,
-    };
-  };
 
   useEffect(() => {
+    const pageSetup = async (target, userName) => {
+      setStatus("FETCHING_AI");
+
+      if (!model.current) model.current = await loadModel();
+      if (!metaData.current) metaData.current = await getMetaData();
+
+      setStatus("FETCHING_TWEETS");
+
+      const response = await fetch(`${API_URL}/${target}`, {
+        method: "post",
+        body: JSON.stringify({ user: userName }),
+      });
+
+      const tweetsParsed = await response.json();
+
+      if (!tweetsParsed.tweets || tweetsParsed.tweets.length === 0) {
+        return null;
+      }
+
+      setTweetCount(tweetsParsed.tweets.length);
+
+      setStatus("FETCHED_TWEETS");
+
+      return {
+        user: tweetsParsed.user,
+        tweets: tweetsParsed.tweets,
+        chart: {
+          datasets: [
+            {
+              label: "sentiment analysis result",
+              data: [positiveCount, negativeCount, neutralCount],
+              backgroundColor: [
+                "rgb(68, 242, 103)",
+                "rgb(240, 104, 77)",
+                "rgb(222, 222, 222)",
+              ],
+              hoverOffset: 4,
+            },
+          ],
+        },
+      };
+    };
+
     if (status === "INITIAL" && userName && target)
       pageSetup(target, userName)
         .then((res) => {
           if (res) {
-            const {
-              positiveCount,
-              negativeCount,
-              neutralCount,
-              chart,
-              tweets,
-              user,
-            } = res;
+            const { chart, tweets, user } = res;
+
+            const tweetsAnalysed = tweets.map((item) => {
+              const score = sentimentAnalysis(
+                item.text,
+                model.current,
+                metaData.current
+              );
+              switch (score) {
+                case "POSITIVE":
+                  setPositiveCount((count) => count + 1);
+                  break;
+                case "NEGATIVE":
+                  setNegativeCount((count) => count + 1);
+                  break;
+                default:
+                  setNeutralCount((count) => count + 1);
+                  break;
+              }
+              return {
+                ...item,
+                score,
+              };
+            });
             setUserData(user);
-            setTweeterData(tweets);
-            setPositiveCount(positiveCount);
-            setNegativeCount(negativeCount);
-            setNeutralCount(neutralCount);
+            setTweeterData(tweetsAnalysed);
             setChartData(chart);
             setStatus("READY");
           } else setStatus("NO_TWEETS");
@@ -165,56 +212,24 @@ function Page({}) {
     case "FETCHED_TWEETS":
       return (
         <DefaultElement>
-          Processing {tweetCount} {target} please be patient ... .
+          Processing {positiveCount + negativeCount + negativeCount}{" "}
+          {tweetCount} {target} please be patient ... .
         </DefaultElement>
       );
+
     case "READY":
       return (
-        <div className="min-h-screen bg-indigo-100">
-          <div className="flex flex-wrap items-center justify-center w-full align-middle flex-column">
-            <div className="flex flex-row justify-between w-11/12 pt-3 ">
-              <span className="p-2">
-                <Avatar
-                  imageSrc={userData.profile_image_url}
-                  userName={userData.name}
-                />
-              </span>
-              <div>
-                <p className="px-1 mt-1 text-lg text-center text-white capitalize bg-blue-600 rounded-lg text-">
-                  {target} : {tweetCount}
-                </p>
-                <p className="px-1 mt-1 text-lg text-green-400 capitalize rounded-lg ">
-                  Positive count : {positiveCount}
-                </p>
-                <p className="px-1 mt-1 text-lg text-red-400 capitalize rounded-lg ">
-                  Negative count : {negativeCount}
-                </p>
-                <p className="px-1 mt-1 text-lg text-gray-600 capitalize rounded-lg ">
-                  Neutral count : {neutralCount}
-                </p>
-              </div>
-
-              <span className="w-32 h-32">
-                <Pie data={chartData} />
-              </span>
-            </div>
-            <div className="flex flex-wrap w-11/12 flex-column ">
-              {TweeterData.map(({ text, id, score }) => {
-                const bgColor = {
-                  NEGATIVE: "bg-red-200",
-                  POSITIVE: "bg-green-200",
-                  NEUTRAL: "bg-white",
-                };
-
-                return (
-                  <span className="w-full py-1 " key={id}>
-                    <Tweet text={text} className={bgColor[score]} />
-                  </span>
-                );
-              })}
-            </div>
-          </div>
-        </div>
+        <Tweets
+          username={userData.name}
+          userImage={userData.profile_image_url}
+          tweetCount={tweetCount}
+          tweetsType={target}
+          positiveCount={positiveCount}
+          negativeCount={negativeCount}
+          neutralCount={neutralCount}
+          chartData={chartData}
+          tweets={TweeterData}
+        />
       );
 
     default:
